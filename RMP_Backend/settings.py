179de7fa7e612c1a4d6203a488e77e61b01cc8ae
Arnav-KUM <arnav20363@iiitd.ac.in>
2023-08-22 19:29:31 +0530

    update

--- RMP_Backend/settings.py
+++ RMP_Backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u+78$+w03y(ab0lkyk$qku+(x(lz^@mm52&kfeel=ny4jef23_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
